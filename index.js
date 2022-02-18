1923988d676d57275fd8acd11b103917e471630c
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-02-18 13:35:34 -0500

    commit tree 1c8eca834fd2f5ee8b0e99214c2cf2237254af1e

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name) {
+   
+      const message = `Hi, my name is ${name}.`; 
+      console.log(message);
+      message = "changed";
+      return message;
+}
+
+introduction(3); 
